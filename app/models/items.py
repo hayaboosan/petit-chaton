1049084faf1292bf5f6b63a591f009900a6237da
hayaboosan <hayaboosan@gmail.com>
2022-06-04 07:50:01 +0900

    [Add]画像説明文カラム追加

--- app/models/items.py
+++ app/models/items.py
@@ -6,15 +6,17 @@ class Item(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255))
+    alt = db.Column(db.String(255))
     text = db.Column(db.String(255))
     shop_url = db.Column(db.String(255))
     image_url = db.Column(db.String(255))
 
     def __init__(
-            self, id=None, title=None, text=None,
+            self, id=None, title=None, alt=None, text=None,
             shop_url=None, image_url=None):
         self.id = id
         self.title = title
+        self.alt = alt
         self.text = text
         self.shop_url = shop_url
         self.image_url = image_url
