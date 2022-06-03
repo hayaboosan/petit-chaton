22f978634d753b6c298a8b77ef5ad7e56743200b
hayaboosan <hayaboosan@gmail.com>
2022-06-03 09:23:20 +0900

    [Add]商品モデルを作成

---
+++ app/models/items.py
@@ -0,0 +1,20 @@
+from app import db
+
+
+class Item(db.Model):
+    __tablename__ = 'items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    text = db.Column(db.String(255))
+    shop_url = db.Column(db.String(255))
+    image_url = db.Column(db.String(255))
+
+    def __init__(
+            self, id=None, title=None, text=None,
+            shop_url=None, image_url=None):
+        self.id = id
+        self.title = title
+        self.text = text
+        self.shop_url = shop_url
+        self.image_url = image_url
