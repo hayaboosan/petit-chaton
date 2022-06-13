3a9059a0d7aff6d363d1d0d7154ca29820ceb1fd
hayaboosan <hayaboosan@gmail.com>
2022-06-14 06:48:00 +0900

    [Change]navbarをモデルから抽出

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 from sqlalchemy import desc
 
-from app.models import Item
+from app.models import Item, NavLink
 
 main = Blueprint('main', __name__, static_folder='static')
 
@@ -9,4 +9,6 @@ main = Blueprint('main', __name__, static_folder='static')
 @main.route('/')
 def home() -> str:
     items = Item.query.order_by(desc(Item.id)).all()
-    return render_template("./main/top_page.html", items=items)
+    nav_links = NavLink.query.order_by(NavLink.left_num).all()
+    return render_template(
+        "./main/top_page.html", items=items, nav_links=nav_links)
