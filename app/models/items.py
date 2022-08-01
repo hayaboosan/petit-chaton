76ed60d22c7076a5e9650af25a52ddcc6ec0fe53
hayaboosan <hayaboosan@gmail.com>
2022-08-01 18:48:43 +0900

    [Add]次世代画像フォーマットのURL用カラム作成

--- app/models/items.py
+++ app/models/items.py
@@ -10,13 +10,17 @@ class Item(db.Model):
     text = db.Column(db.Text)
     shop_url = db.Column(db.Text)
     image_url = db.Column(db.Text)
+    avif_url = db.Column(db.Text)
+    webp_url = db.Column(db.Text)
 
     def __init__(
             self, id=None, title=None, alt=None, text=None,
-            shop_url=None, image_url=None):
+            shop_url=None, image_url=None, avif_url=None, webp_url=None):
         self.id = id
         self.title = title
         self.alt = alt
         self.text = text
         self.shop_url = shop_url
         self.image_url = image_url
+        self.avif_url = avif_url
+        self.webp_url = webp_url
